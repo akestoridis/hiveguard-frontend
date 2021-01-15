2e277f32ce8102e75162df623da0973c599156e0
Dimitrios-Georgios Akestoridis <akestoridis@cmu.edu>
2021-01-15 01:22:05 -0500

    Start working on the layout of the Home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,14 +7,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import WIDSSensors from '../segments/WIDSSensors';
 
 function Home({ inspectionAPI }) {
   return (
-    <Container>
-      <WIDSSensors
-        url={`${inspectionAPI}/wids-sensors`}
-      />
+    <Container fluid>
+      <Jumbotron align="center">
+        <h1>Home Page</h1>
+      </Jumbotron>
+      <Row>
+        <Col />
+        <Col />
+        <Col>
+          <WIDSSensors
+            url={`${inspectionAPI}/wids-sensors`}
+          />
+        </Col>
+      </Row>
     </Container>
   );
 }
--- src/components/segments/WIDSSensors.jsx
+++ src/components/segments/WIDSSensors.jsx
@@ -6,9 +6,27 @@
 
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
-import { ArrowClockwise } from 'react-bootstrap-icons';
+import {
+  ArrowClockwise,
+  PlusCircleFill,
+  XCircleFill,
+} from 'react-bootstrap-icons';
+
+const dataEntryStyle = {
+  textAlign: 'left',
+  verticalAlign: 'middle',
+};
+
+const actionEntryStyle = {
+  textAlign: 'center',
+  verticalAlign: 'middle',
+  width: '2px',
+};
 
 function WIDSSensors(props) {
   const [fetchState, setFetchState] = useState('Fetching data...');
@@ -45,41 +63,78 @@ function WIDSSensors(props) {
       dataState,
       (row) => (
         <tr key={row.wids_sensor_id}>
-          <td>{row.wids_sensor_id}</td>
-          <td>{row.wids_sensor_api}</td>
+          <td style={dataEntryStyle}>
+            {row.wids_sensor_id}
+          </td>
+          <td style={dataEntryStyle}>
+            {row.wids_sensor_api}
+          </td>
+          <td style={actionEntryStyle}>
+            <Button variant="danger">
+              <XCircleFill />
+            </Button>
+          </td>
         </tr>
       ),
     ));
   }, [dataState]);
 
   return (
-    <>
-      <h3 align="center">
-        Registered WIDS Sensors
-      </h3>
-      <p style={{ textAlign: 'right', fontStyle: 'italic' }}>
-        <small>
-          {fetchState}
-        </small>
-        {' '}
-        <Button variant="primary" onClick={() => fetchData()}>
-          <ArrowClockwise />
-        </Button>
-      </p>
-      <div style={{ overflow: 'auto', height: '400px' }}>
-        <Table striped bordered>
-          <thead>
-            <tr>
-              <th>WIDS Sensor ID</th>
-              <th>WIDS Sensor API</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableRowsState}
-          </tbody>
-        </Table>
-      </div>
-    </>
+    <Container fluid>
+      <Row noGutters className="align-items-end">
+        <Col>
+          <p style={{ textAlign: 'left' }}>
+            <b style={{ verticalAlign: 'bottom', fontSize: 'x-large' }}>
+              Registered WIDS Sensors
+            </b>
+          </p>
+        </Col>
+        <Col>
+          <p style={{ textAlign: 'right' }}>
+            <i style={{ verticalAlign: 'bottom', fontSize: 'small' }}>
+              {fetchState}
+            </i>
+            {' '}
+            <Button variant="primary" onClick={() => fetchData()}>
+              <ArrowClockwise />
+            </Button>
+          </p>
+        </Col>
+      </Row>
+      <Row noGutters className="align-items-start">
+        <Col>
+          <div style={{ overflow: 'auto', height: '400px' }}>
+            <Table striped bordered>
+              <thead>
+                <tr>
+                  <th style={dataEntryStyle}>
+                    WIDS Sensor ID
+                  </th>
+                  <th style={dataEntryStyle}>
+                    WIDS Sensor API
+                  </th>
+                  <th style={actionEntryStyle}>
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableRowsState}
+                <tr>
+                  <td style={dataEntryStyle} />
+                  <td style={dataEntryStyle} />
+                  <td style={actionEntryStyle}>
+                    <Button variant="primary">
+                      <PlusCircleFill />
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
